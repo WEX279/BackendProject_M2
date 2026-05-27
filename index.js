8c5ea5fa0bdf1f69548a5bfe8dc522bedf44eb96
WEX_279 <legazyuri279@gmail.com>
2026-05-27 23:16:06 +0200

    feat: add methods and fix bugs

--- index.js
+++ index.js
@@ -1,64 +1,109 @@
-const express = require ("express");
+const express = require("express");
 
-const app = express()  //importantisimo
+const app = express(); //importantisimo
 const PORT = 3000;
-// const cors = require(cors)
 
-app.use(express.json())
-app.use((req, res, next)=> {
-    console.log(`[${new Date().toISOString()}]${req.method}${req.url}`)
-    next();
-})
+const fs = require("node:fs").promises;
+const path = require("node:path");
+const RUTA = path.join(__dirname, "manga.json");
 
+async function loadManga() {
+  const content = await fs.readFile(RUTA, "utf-8");
+  return JSON.parse(content);
+}
 
-const manga = [
-        { id: 1, name: "Berserk", author: "Kentaro Miura", release: "Aug 1989"},
-        { id: 2, name: "JoJo`s Bizarre Adventure: Steel Ball Run", author: "Hirohiko Araki", release: "Jan 2004"},
-        { id: 3, name: "Vagabond", author: "Takehiko Inoue", release: "Sep 1998"},
-        { id: 4, name: "One Piece", author: "Eiichiro Oda", release: "Jul 1997"},
-        { id: 5, name: "Guimi Zhi Zhu", author: "Ai Qianshui de Wuzei", release: "May 2020"}
-]
+async function saveManga(manga) {
+  await fs.writeFile(RUTA, JSON.stringify(manga, null, 2), "utf-8");
+}
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}]${req.method}${req.url}`);
+  next();
+});
 
-// app.use(
-//     cors({
-//         origin: "http://localhost:3000",
-//     })
-// )
-
-app.get("/api/manga", (req, res) =>{
-    res.json(manga)
-})
+app.get("/api/manga", async (req, res) => {
+  const manga = await loadManga();
+  res.json(manga);
+});
 
 // Conseguir un id concreto:
-app.get("/api/manga/:id", (req,res) =>{
-    const id = Number(req.params.id)
-    const data = manga.find((m) => m.id === id)
+app.get("/api/manga/:id", async (req, res) => {
+  const manga = await loadManga();
+  const id = Number(req.params.id);
+  const data = manga.find((m) => m.id === id);
 
-    if (!data){
-        return res.status(404).json({ error: "Id not found"})
-    } 
+  if (!data) {
+    return res.status(404).json({ error: "Id not found" });
+  }
 
-    res.json(data)
-})
+  res.json(data);
+});
 
 // Añadir un nuevo objeto al array manga
-app.post("/api/manga", (req, res) =>{
-    const {name, author, release} = req.body
-    console.log(name, author, release)
-    if (!name || !author || !release){
-        return res.status(400).json({error: "missing name, author or release date"})
-    }
-    const newNote = {
-        id: manga.length++,
-        name: name,
-        author: author,
-        release: release
-    }
+app.post("/api/manga", async (req, res) => {
+  const manga = await loadManga();
 
-    manga.push(newNote)
-    res.status(201).json(newNote)
+  const { name, author, release, genre } = req.body;
+  console.log(name, author, release)
+  if (!name || !author || !release || !genre) {
+    return res
+      .status(400)
+      .json({ error: "missing name, author, release date or genre" });
+  }
+  const newNote = {
+    id: manga.length + 1,
+    name: name,
+    author: author,
+    release: release,
+    genre: genre
+  };
+
+  console.log(newNote)
+  manga.push(newNote);
+  await saveManga(manga);
+  res.status(201).json(newNote);
+});
+
+app.put("api/manga/:id", async (req, res) =>{
+  const id = Number(req.params.id);
+  const { name, author, release, genre} = req.body;
+
+  if (!name || !author || !release || !genre){
+    return res.status(400).json({error: "missing name, author, release date or genre"})
+  }
+
+  const manga = await loadManga();
+  const indice = manga.findIndex((m)=> m.id === id);
+
+  if (indice === -1)
+    return res.status(404).json({error: "id not found"})
+
+  manga[indice] = {name, author, release, genre};
+  await saveManga(manga)
+  res.json(manga[indice])
+
 })
 
-app.listen(PORT, () =>{
-    console.log(`servidor funcionando en http://localhost:${PORT}`);
+app.delete("/api/manga/:id", async (req, res)=>{
+  const id = Number(req.params.id);
+  console.log(id)
+  const manga = await loadManga();
+  const indice = manga.findIndex((m)=> m.id === id);
+  console.log("indice conseguido")
+  if (indice === -1){
+    return res.status(404).json({ error: "id not found"})
+  }
+
+  const [deleted] = manga.splice(indice, 1);
+  console.log(indice)
+  
+  await saveManga(manga);
+  console.log("manga borrado")
+  res.json({message: "Manga deleted succesfully!", manga: deleted  })
+
 })
+
+
+app.listen(PORT, () => {
+  console.log(`servidor funcionando en http://localhost:${PORT}`);
+}) 
