0a7c1055c12686dc892a3e307a663bb976e4cd30
WEX_279 <legazyuri279@gmail.com>
2026-06-26 18:52:02 +0200

    patch cors

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ import usersRoutes from "./src/routers/users.routers.js"
 
 
 app.use(cors(
-    {
-    origin: [
-        'https://backendproject-m2.onrender.com/api',
-        'http://localhost:5173'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    allowedHeaders:[
-        "content-type",
-        // "Authorization",
-        "access-control-allow-origin"
-    ] 
+//     {
+//     origin: [
+//         'http://localhost:5173'
+//     ],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//     allowedHeaders:[
+//         "content-type",
+//         // "Authorization",
+//         "access-control-allow-origin"
+//     ] 
 
-}
+// }
 ))
 
 app.use(express.json()) // req => usuario
